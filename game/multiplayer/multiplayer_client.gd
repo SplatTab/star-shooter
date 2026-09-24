6f486463a27db0c27ad0268dc6255399c4e883c9
SplatTab <skidm44@gmail.com>
2026-09-23 23:24:55 -0500

    audit fix

--- game/multiplayer/multiplayer_client.gd
+++ game/multiplayer/multiplayer_client.gd
@@ -1,7 +1,7 @@
 extends "ws_webrtc_client.gd"
 
 # Update this endpoint string to point to your live deployment URL (Render, Back4app, etc.)
-const host := "wss://starshooter-7604gyw5.b4a.run"
+const host := "wss://star-shooter.onrender.com"
 var rtc_mp := WebRTCMultiplayerPeer.new()
 var sealed: bool = false
 
